574adae9501d6f5319018f4619d903b73d82192f
Dileep <dileepkumarreddy9797@gmail.com>
2023-12-09 15:41:11 -0500

    implementing routing and enabling store dev tools

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from './counter/counter/counter.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CounterComponent],
+  imports: [CommonModule, CounterComponent, RouterModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
